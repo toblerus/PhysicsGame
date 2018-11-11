bfbc5fa39c66dbe8bb525117b398438800c2106a
Tobi <toblerus@hotmail.de>
2018-11-11 23:46:45 +0100

    Initial commit

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+    public int LevelLength;
+    public GameObject LevelParent;
+    public GameObject[] LevelPrefabs;
+
+	// Use this for initialization
+	void Start ()
+    {
+        GenerateLevel();
+
+    }
+	
+    public void GenerateLevel()
+    {
+        for (int i = 0; i <= LevelLength; i++)
+        {
+            float LevelPrefabIndex = Random.Range(float 0,float 1)*10;
+            Debug.Log(LevelPrefabIndex);
+            //test
+        }
+    }
+}
