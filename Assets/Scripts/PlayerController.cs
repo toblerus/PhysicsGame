e722abc624a292ffaacfc75950f06cbcc437ad8f
toblerus <37512320+toblerus@users.noreply.github.com>
2018-11-12 15:50:45 +0100

    First Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    
+    public bool buttonIsPressed;
+
+    public void Move()
+    {
+        GetComponent<ConstantForce2D>().enabled = true;
+    }
+
+    public void DisableForce()
+    {
+        GetComponent<ConstantForce2D>().enabled = false;
+    }
+
+    public void Update()
+    {
+        if (buttonIsPressed == true)
+        {
+            Move();
+        }
+        else
+        {
+            DisableForce();
+        }
+    }
+
+    public void buttonPress() { buttonIsPressed = true; }
+    public void buttonUp() { buttonIsPressed = false; }
+
+}
