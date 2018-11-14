8cdbadc53a84a1c178f195e166342c03efd5af3a
toblerus <37512320+toblerus@users.noreply.github.com>
2018-11-14 08:59:15 +0100

    Status update

---
+++ Assets/Scripts/CameraPosition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour {
+
+    public GameObject Camera;
+    public GameObject Player;
+	
+	// Update is called once per frame
+	void Update () {
+        Camera.transform.position = new Vector3 (Player.transform.position.x, Player.transform.position.y, -10);
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     
     public bool buttonIsPressed;
+    public float IncreaseSpeed;
+
+    private float nextActionTime = 0.0f;
+    public float period = 0.5f;
 
     public void Move()
     {
-        GetComponent<ConstantForce2D>().enabled = true;
-    }
+        GetComponent<ConstantForce2D>().enabled = true;    }
 
     public void DisableForce()
     {
         GetComponent<ConstantForce2D>().enabled = false;
+        GetComponent<ConstantForce2D>().force = new Vector2(0, -5);
     }
 
     public void Update()
@@ -21,11 +25,21 @@ public class PlayerController : MonoBehaviour {
         if (buttonIsPressed == true)
         {
             Move();
+            if (Time.time > nextActionTime)
+            {
+                nextActionTime += period;
+                increaseGravity();
+            }
         }
         else
         {
             DisableForce();
         }
+    }
+
+    public void increaseGravity()
+    {
+        GetComponent<ConstantForce2D>().force = new Vector2(0, GetComponent<ConstantForce2D>().force.y - 1);
     }
 
     public void buttonPress() { buttonIsPressed = true; }
